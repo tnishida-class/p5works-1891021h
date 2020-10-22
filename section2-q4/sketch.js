88b2df4a5bf51b5060713a525d52878f83a1996f
1891021h <1891021h@stu.kobe-u.ac.jp>
2020-10-22 22:11:15 +0900

    greek start

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -2,14 +2,20 @@
 function setup() {
   const blue = color(0, 51, 160);
   createCanvas(270, 180);
-  // noStroke();
+  noStroke();
   background(255);
+  fill(blue);
 
   let d = height / 9; // 縞1本の太さ
 
   for(let i = 0; i < 9; i++){
     // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
     rect(0, i * d, width, (i + 1) * d);
+    if(i % 2 == 1){
+      fill(blue);
+    } else {
+      fill(255);
+    }
   }
 
   fill(blue);
@@ -18,4 +24,6 @@ function setup() {
 
   fill(255);
   // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
+  rect(0, 2 * d, width / 2.72, d);
+  rect(width / 1.36, 0, d, 3 * d);
 }
